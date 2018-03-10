11b89ab3d3a0bd84d31891b679d7d5317c7f43d9
SamkitS <samkitshah.nmims@gmail.com>
2018-03-10 14:25:35 +0530

    git commit 2

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,32 @@
+
+const {MongoClient, ObjectID} = require('mongodb'); //ES6 Object Destructuring 
+
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if(err) {
+        return console.log('Unable to Connect to mongodb server');
+    }
+    console.log('connected to MongoDB server');
+    const db = client.db('TodoApp');
+    
+    //deleteMany
+    // db.collection('Users').deleteMany({name: 'Samkit'}).then((result) => {
+    //     console.log(result);
+
+    // });
+
+    //deleteOne
+    db.collection('Users').deleteOne({_id: new ObjectID("5a9bc4a60cec2c3ed5358eae") }).then((result) => {
+        console.log(result);
+    });
+   
+    //findOneAndDelete
+    // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+    //     console.log(result);
+    // });
+
+
+    // client.close();
+})
+
