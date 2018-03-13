04261e09ed1d56b50b76eced1b84fdeef5a74bb6
SamkitS <samkitshah.nmims@gmail.com>
2018-03-13 17:40:41 +0530

    Heroku upload t2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise =  global.Promise;
-mongoose.connect( 'mongodb://samkit:28618302@ds113169.mlab.com:13169/todonode'
-     || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://samkit:28618302@ds113169.mlab.com:13169/todonode');
 
 module.exports = {mongoose};
