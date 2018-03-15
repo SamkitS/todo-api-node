8ba28b709c9c9f601ba714b1f9bbcb677a29a791
SamkitS <samkitshah.nmims@gmail.com>
2018-03-15 15:24:25 +0530

    Heroku setup 3

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,16 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise =  global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://samkit:28618302@ds113169.mlab.com:13169/todonode');
+
+let db = {
+    localhost: 'mongodb://localhost:27017/TodoApp',
+    mlab: 'mongodb://samkit:28618302@ds113169.mlab.com:13169/todonode'
+  };
+
+  mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
+  
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
+
+// 
