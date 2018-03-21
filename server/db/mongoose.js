f0f189cad98f585b75d870953f5a775e84be4556
SamkitS <samkitshah.nmims@gmail.com>
2018-03-21 18:15:51 +0530

    Set up different test environment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,14 +2,14 @@ var mongoose = require('mongoose');
 
 mongoose.Promise =  global.Promise;
 
-let db = {
-    localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://samkit:28618302@ds113169.mlab.com:13169/todonode'
-  };
+// let db = {
+//     localhost: 'mongodb://localhost:27017/TodoApp',
+//     mlab: 'mongodb://samkit:28618302@ds113169.mlab.com:13169/todonode'
+//   };
 
-  mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
+//   mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
 
